e4707eecba6e30d1bf660b3ee8800cc3d2e9295e
Ethan Dinh <ethandinh@Ethans-MacBook-Pro.local>
2022-04-13 16:10:08 -0700

    uniprot finished

--- Uniprot.py
+++ Uniprot.py
@@ -4,9 +4,11 @@ Generating a list of UNIPROT IDs from GENOME NAMES
 
 # Importing Libraries
 import csv
+from typing import List
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By 
 
 # Importing CSV File for GENES
 def import_csv(filename):
@@ -15,15 +17,46 @@ def import_csv(filename):
         header = next(csv_reader)
         genes = []
         for row in csv_reader:
-            genes.append(row)
+            genes.append(row[0])
 
     return genes   
 
+# Configuring selenium
+chromedriver = "/Users/ethandinh/Desktop/Personal/Automated Scripts/chromedriver"
+option = webdriver.ChromeOptions()
+option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+option.add_experimental_option("excludeSwitches", ['enable-automation'])
+option.headless = True
+s = Service(chromedriver)
+driver = webdriver.Chrome(service=s, options=option)
+
+def scrape(name) -> List:
+    
+    search_area = driver.find_element(By.ID, "query")
+    search_area.clear()
+    search_area.send_keys(name)
+    search_area.send_keys(Keys.RETURN)
+
+    for i in range(1,5):
+        entry = driver.find_element(By.XPATH, f"//table/tbody/tr[{i}]/td[3]").text
+        if "HUMAN" in entry:
+            return driver.find_element(By.XPATH, f"//table/tbody/tr[{i}]/td[2]").text
+
 # Main Function --------------------------------
 def main():
-    filename = input("Filename:")
+    #filename = input("Filename: ")
+    filename = "gene_table.csv"
     gene_names = import_csv(filename)
-    print(gene_names)
+    driver.get("https://www.uniprot.org/")
+    
+    rows = []
+    for name in gene_names:
+        rows.append([scrape(name)])
+        print(rows)
+
+    with open("Uniprot IDs") as outFile:
+        write = csv.writer(outFile, delimiter=",")
+        write.writerows(rows)
 
 if __name__ == "__main__":
     main()
